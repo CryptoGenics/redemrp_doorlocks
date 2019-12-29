d722648f4e986d2cd782831d010d5f0e77e72d24
CryptoGenics <45253987+CryptoGenics@users.noreply.github.com>
2019-12-29 15:07:13 -0600

    restructured / multi-door

--- client/main.lua
+++ client/main.lua
@@ -1,52 +1,103 @@
+-- Get objects every second, instead of every frame
+Citizen.CreateThread(function()
+	while true do
+		for _,doorID in ipairs(Config.DoorList) do
+			if doorID.doors then
+				for k,v in ipairs(doorID.doors) do
+					if not v.object or not DoesEntityExist(v.object) then
+						local shapeTest = StartShapeTestBox(v.objCoords, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, true, 16)
+						--v.object = GetClosestObjectOfType(v.objCoords, 1.0, GetHashKey(v.objName), false, false, false)
+						local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
+						v.object = entityHit
+					end
+				end
+			else
+				if not doorID.object or not DoesEntityExist(doorID.object) then
+					local shapeTest = StartShapeTestBox(doorID.objCoords, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, true, 16)
+					local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
+					doorID.object = entityHit
+				end
+			end
+		end
+
+		Citizen.Wait(1000)
+	end
+end)
+
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
-		local playerCoords = GetEntityCoords(PlayerPedId())
+		local playerCoords, letSleep = GetEntityCoords(PlayerPedId()), true
 
-		for i=1, #Config.DoorList do
-			local doorID   = Config.DoorList[i]
-			local distance = GetDistanceBetweenCoords(playerCoords, doorID.doorCoords.x, doorID.doorCoords.y, doorID.doorCoords.z, true)
+		for k,doorID in ipairs(Config.DoorList) do
+			local distance
 
-			local maxDistance = 2
+			if doorID.doors then
+				distance = #(playerCoords - doorID.doors[1].objCoords)
+			else
+				distance = #(playerCoords - doorID.objCoords)
+			end
+
+			local maxDistance, displayText = 1.25, 'unlocked'
+
 			if doorID.distance then
 				maxDistance = doorID.distance
 			end
 
 			if distance < 50 then
-				ApplyDoorState(doorID)
-				if distance < maxDistance then
+				letSleep = false
 
-					local displayText = 'Unlocked'
-					if doorID.locked then
-						displayText = 'Locked'
+				if doorID.doors then
+					for _,v in ipairs(doorID.doors) do
+						FreezeEntityPosition(v.object, doorID.locked)
+						DoorSystemSetDoorState(v.object, doorID.locked)
+
+
+						if doorID.locked and v.objYaw and GetEntityRotation(v.object).z ~= v.objYaw then
+							SetEntityRotation(v.object, 0.0, 0.0, v.objYaw, 2, true)
+						end
 					end
+				else
+					FreezeEntityPosition(doorID.object, doorID.locked)
+					DoorSystemSetDoorState(doorID.object, doorID.locked)
 
-					DrawText3D(doorID.textCoords.x, doorID.textCoords.y, doorID.textCoords.z, displayText)
-					
-					if IsControlJustPressed(2, 0xE8342FF2) then -- Hold ALT
-						TriggerEvent("redemrp_doorlocks:updatedoor", GetPlayerServerId(), i)
+
+					if doorID.locked and doorID.objYaw and GetEntityRotation(doorID.object).z ~= doorID.objYaw then
+						SetEntityRotation(doorID.object, 0.0, 0.0, doorID.objYaw, 2, true)
 					end
 				end
 			end
+
+			if distance < maxDistance then
+
+				if doorID.locked then
+					displayText = 'locked'
+				end
+
+				DrawText3D(doorID.textCoords.x, doorID.textCoords.y, doorID.textCoords.z, displayText)
+
+				if IsControlJustPressed(2, 0xE8342FF2) then -- Hold ALT
+					TriggerEvent("redemrp_doorlocks:updatedoor", GetPlayerServerId(), k, doorID.locked)
+				end
+			end
+		end
+
+		if letSleep then
+			Citizen.Wait(500)
 		end
 	end
 end)
 
 RegisterNetEvent('redemrp_doorlocks:updatedoor')
-AddEventHandler('redemrp_doorlocks:updatedoor', function(source, door) 
-    TriggerServerEvent("redemrp_doorlocks:updatedoorsv", source, door, function(cb) end) --Perms check
+AddEventHandler('redemrp_doorlocks:updatedoor', function(source, door, lock) 
+    TriggerServerEvent("redemrp_doorlocks:updatedoorsv", source, door, lock, function(cb) end) --Perms check
 end)
 
 RegisterNetEvent('redemrp_doorlocks:changedoor')
-AddEventHandler('redemrp_doorlocks:changedoor', function(doorID) 
+AddEventHandler('redemrp_doorlocks:changedoor', function(doorID, lock)
 	local name   = Config.DoorList[doorID]
 	name.locked = not name.locked
-	TriggerServerEvent('redemrp_doorlocks:updateState',GetPlayerServerId(), doorID, name.locked, function(cb) end)
-end)
-
-RegisterNetEvent('redemrp_doorlocks:setState')
-AddEventHandler('redemrp_doorlocks:setState', function(doorID, state)
-	Config.DoorList[doorID].locked = state
+	TriggerServerEvent('redemrp_doorlocks:updateState', doorID, name.locked, function(cb) end)
 end)
 
 function DrawText3D(x, y, z, text)
@@ -63,15 +114,8 @@ function DrawText3D(x, y, z, text)
     DrawSprite("generic_textures", "hud_menu_4a", _x, _y+0.0125,0.015+ factor, 0.03, 0.1, 100, 1, 1, 190, 0)
 end
 
-function ApplyDoorState(doorID)
-	local shapeTest = StartShapeTestBox(doorID.doorCoords.x, doorID.doorCoords.y, doorID.doorCoords.z, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, true, 16)
-    local rtnVal, hit, endCoords, surfaceNormal, entityHit = GetShapeTestResult(shapeTest)
-	FreezeEntityPosition(entityHit, doorID.locked)
-	DoorSystemSetDoorState(entityHit, doorID.locked)
-	if doorID.locked then
-		SetEntityRotation(entityHit, 0.0, 0.0, doorID.objYaw, 1, true)
-		if GetEntityRotation(entityHit).z ~= doorID.objYaw then
-			SetEntityRotation(entityHit, 0.0, 0.0, doorID.objYaw, 1, true)
-		end
-	end
-end
+-- Set state for a door
+RegisterNetEvent('redemrp_doorlocks:setState')
+AddEventHandler('redemrp_doorlocks:setState', function(doorID, state)
+	Config.DoorList[doorID].locked = state
+end)
